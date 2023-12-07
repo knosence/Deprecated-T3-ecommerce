7fb5b2e4030618f0005f393bc2e2231e3a826d8b
NaDario Seays <knosence@pop-os.localdomain>
2023-12-07 17:10:49 -0500

    adding nav bar and new model storing listings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,12 @@ import { api } from "~/utils/api";
 
 import { ClerkProvider } from "@clerk/nextjs";
 import "~/styles/globals.css";
+import { NavBar } from "~/components/NavBar";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+        <NavBar/>
       <Component {...pageProps} />;
     </ClerkProvider>
   );
